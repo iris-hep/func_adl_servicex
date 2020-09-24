90157b2298c8da7359dc3fd579bdef98f269b6d7
Gordon Watts <gordon@gordonandpaula.net>
2020-09-23 22:35:58 -0400

    Update test xaod script
    This now works quite well!

--- scripts/run_xaod.py
+++ scripts/run_xaod.py
@@ -4,7 +4,7 @@ from func_adl_servicex import ServiceXSourceXAOD
 
 
 def simple_call():
-    dataset_xaod = "user.kchoi:user.kchoi.ttHML_80fb_ttbar"
+    dataset_xaod = "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00"
     ds = ServiceXSourceXAOD(dataset_xaod)
     data = ds \
         .SelectMany('lambda e: (e.Jets("AntiKt4EMTopoJets"))') \
@@ -17,4 +17,9 @@ def simple_call():
 
 
 if __name__ == "__main__":
+    # import logging
+    # ch = logging.StreamHandler()
+    # ch.setLevel(logging.DEBUG)
+    # logging.getLogger('servicex').setLevel(logging.DEBUG)
+    # logging.getLogger('servicex').addHandler(ch)
     simple_call()
