f108f3a5f1ad9aced97c511cb3881025cb62e0fe
Gordon Watts <gordon@gordonandpaula.net>
2020-10-20 21:56:11 -0400

    Fix the standalone uproot code up
    Now that we have a working backend!

--- scripts/run_uproot.py
+++ scripts/run_uproot.py
@@ -1,15 +1,11 @@
 # Show we can run up-root
 
-from servicex import ServiceXDataset
 from func_adl_servicex import ServiceXSourceUpROOT
 
 
 def simple_call():
     dataset_uproot = "user.kchoi:user.kchoi.ttHML_80fb_ttbar"
-    uproot_transformer_image = "sslhep/servicex_func_adl_uproot_transformer:issue6"
-
-    sx_dataset = ServiceXDataset(dataset_uproot, image=uproot_transformer_image)
-    ds = ServiceXSourceUpROOT(sx_dataset, "nominal")
+    ds = ServiceXSourceUpROOT(dataset_uproot, "nominal")
     data = ds.Select("lambda e: {'lep_pt_1': e.lep_Pt_1, 'lep_pt_2': e.lep_Pt_2}") \
         .AsParquetFiles('junk.parquet') \
         .value()
@@ -18,4 +14,9 @@ def simple_call():
 
 
 if __name__ == "__main__":
+    # import logging
+    # ch = logging.StreamHandler()
+    # ch.setLevel(logging.DEBUG)
+    # logging.getLogger('servicex').setLevel(logging.DEBUG)
+    # logging.getLogger('servicex').addHandler(ch)
     simple_call()
