2181be03c6bccd7fa6bb784ded4a4831524988ca
Gordon Watts <gordon@gordonandpaula.net>
2022-03-14 02:16:40 +0100

    Fix up dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name="func_adl_servicex",
       license="TBD",
       test_suite="tests",
       install_requires=[
-          "func_adl>=2.4a1, <3.0",
+          "func_adl>=3.0b1",
           "qastle>=0.10, <1.0",
           "servicex>=2.4, <3.0a1"
       ],
@@ -48,7 +48,7 @@ setup(name="func_adl_servicex",
               'asyncmock'
           ],
           'local': [
-              'func_adl_xAOD[local]==1.5b4',
+              'func_adl_xAOD[local]>=2.0b1',
           ],
       },
       classifiers=[
