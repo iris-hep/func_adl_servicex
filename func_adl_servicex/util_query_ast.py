30e498cc10ba367e81795f0823f9385a381dd1f3
Gordon Watts <gwatts@uw.edu>
2022-10-29 16:30:53 -0700

    Make sure AsParquetFiles works correctly with uproot (#53)
    * Fixes #52 - AsParquetFiles now works properly with uproot
    * Technical Debt:
       * Fixed up some tests to make them more robust
       * Black is now pulled for the dev version of this
       * `vscode` coverage task now work with latest version of `coverage`.
    
    For the future:
    
    * We need to change the symantics of the AsParquetFiles and AsROOTFiles - with a depreciation warning of some sort. No column names shoudl be in here - use a dict instead. This is a [future change](https://github.com/iris-hep/func_adl/issues/71).

--- func_adl_servicex/util_query_ast.py
+++ func_adl_servicex/util_query_ast.py
@@ -1,9 +1,9 @@
 import ast
-from typing import cast
+from typing import Optional, cast
 
 
 def has_col_names(a: ast.AST) -> bool:
-    '''Determine if any column names were specified
+    """Determine if any column names were specified
     in this request.
 
     Args:
@@ -11,12 +11,12 @@ def has_col_names(a: ast.AST) -> bool:
 
     Returns:
         bool: True if no column names were specified, False otherwise.
-    '''
+    """
     assert isinstance(a, ast.Call)
     func_ast = a
     top_function = cast(ast.Name, a.func).id
 
-    if top_function == 'ResultAwkwardArray':
+    if top_function == "ResultAwkwardArray":
         if len(a.args) >= 2:
             cols = a.args[1]
             if isinstance(cols, ast.List):
@@ -28,7 +28,7 @@ def has_col_names(a: ast.AST) -> bool:
         assert isinstance(func_ast, ast.Call)
 
     top_function = cast(ast.Name, func_ast.func).id
-    if top_function not in ['Select', 'SelectMany']:
+    if top_function not in ["Select", "SelectMany"]:
         return False
 
     # Grab the lambda and see if it is returning a dict
@@ -43,3 +43,36 @@ def has_col_names(a: ast.AST) -> bool:
     # as we can tell.
 
     return False
+
+
+def has_tuple(a: ast.AST) -> bool:
+    """Determine if this query used tuples in its final result
+
+    NOTE: This can't do depth searches in a really complex
+    query - then you need to follow best practices.
+
+    Args:
+        a (ast.AST): The query
+
+    Returns:
+        bool: True if the final Select statement has tuples or not.
+    """
+
+    def find_Select(a: ast.AST) -> Optional[ast.Call]:
+        # Descent the call chain until we find a Select.
+        while isinstance(a, ast.Call):
+            if isinstance(a.func, ast.Name):
+                if cast(ast.Name, a.func).id == "Select":
+                    return a
+            a = a.args[0]
+        return None
+
+    select_statement = find_Select(a)
+    if select_statement is None:
+        return False
+
+    # Ok - we have a select statement. Now we need to see if it is returning a tuple.
+    func_called = select_statement.args[1]
+    assert isinstance(func_called, ast.Lambda)
+    body = func_called.body
+    return isinstance(body, ast.Tuple)
